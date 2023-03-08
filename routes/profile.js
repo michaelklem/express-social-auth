8c3c9062d5a0dad4f13e60e884902debd5c1aeb0
Michael Klem <michael.klem@gmail.com>
2023-03-08 09:11:51 -0800

    Initial commit. Able to login via username and password or google auth

---
+++ routes/profile.js
@@ -0,0 +1,36 @@
+var express = require('express');
+var db = require('../db');
+
+function fetchTodos(req, res, next) {
+  db.all('SELECT * FROM todos WHERE owner_id = ?', [
+    req.user.id
+  ], function(err, rows) {
+    if (err) { return next(err); }
+    
+    var todos = rows.map(function(row) {
+      return {
+        id: row.id,
+        title: row.title,
+        completed: row.completed == 1 ? true : false,
+        url: '/' + row.id
+      }
+    });
+    res.locals.todos = todos;
+    res.locals.activeCount = todos.filter(function(todo) { return !todo.completed; }).length;
+    res.locals.completedCount = todos.length - res.locals.activeCount;
+    next();
+  });
+}
+
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  if (!req.user) { return res.render('home'); }
+  next();
+},  function(req, res, next) {
+  res.locals.filter = null;
+  res.render('profile', { user: req.user });
+});
+
+module.exports = router;
